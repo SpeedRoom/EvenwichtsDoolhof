acd9ebe6a3be7dc4645193d6fc38136f94acf2b5
devEmiel <105589240+devEmiel@users.noreply.github.com>
2023-02-21 15:38:18 +0100

    fqd

--- Test stepper motor/src/main.cpp
+++ Test stepper motor/src/main.cpp
@@ -8,6 +8,32 @@ const int stepsPerRevolution = 3200;
 void setup()
 {
 	// Declare pins as Outputs
+    pinMode(2,INPUT);
+	pinMode(stepPin, OUTPUT);
+	pinMode(dirPin, OUTPUT);
+	pinMode(4,INPUT_PULLUP);
+    Serial.begin(9600);
+}
+void loop()
+{
+	while(digitalRead(4)){
+	Serial.println(analogRead(2));
+    delay(100);
+	}
+    Serial.println(digitalRead(4));
+}
+
+/*
+#include <Arduino.h>
+
+// Define pin connections & motor's steps per revolution
+const int dirPin = 18;
+const int stepPin = 19;
+const int stepsPerRevolution = 3200;
+
+void setup()
+{
+	// Declare pins as Outputs
 	pinMode(stepPin, OUTPUT);
 	pinMode(dirPin, OUTPUT);
 }
@@ -38,4 +64,4 @@ void loop()
 		delayMicroseconds(1000);
 	}
 	delay(1000); // Wait a second
-}
+} */
---
+++ Test stepper motor/src/test.cpp
@@ -0,0 +1,22 @@
+/*
+#include <Arduino.h>
+
+// Define pin connections & motor's steps per revolution
+const int dirPin = 18;
+const int stepPin = 19;
+const int stepsPerRevolution = 3200;
+
+void setup()
+{
+	// Declare pins as Outputs
+    pinMode(2,INPUT);
+	pinMode(stepPin, OUTPUT);
+	pinMode(dirPin, OUTPUT);
+    Serial.begin(9600);
+}
+void loop()
+{
+    Serial.println(analogRead(2));
+    delay(100);
+	
+}*/
