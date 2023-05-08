0fafbe2875a97dac70d33ab89dfc58dafd80be76
devEmiel <105589240+devEmiel@users.noreply.github.com>
2023-05-08 17:31:18 +0200

    MQTT fixed

--- CODE_evenwichtsdoolhof/src/main.cpp
+++ CODE_evenwichtsdoolhof/src/main.cpp
@@ -13,7 +13,7 @@ OTAlib ota("NETGEAR68", "excitedtuba713");
 //MQTT -
 #define SSID          "NETGEAR68"
 #define PWD           "excitedtuba713"
-#define MQTT_SERVER   "192.168.0.190"  
+#define MQTT_SERVER   "192.168.1.61"  
 #define MQTT_PORT     1883
 #define topic  "esp_doolhof/output"
 
@@ -31,14 +31,16 @@ const int dirPinx = 33;
 const int stepPinx = 32;
 const int switchy = 14;
 const int switchx = 27;
-const int poty = 26;
-const int potx = 25;
+//const int poty = 26;
+//const int potx = 25;
+const int poty = 39;
+const int potx = 35;
 int posy = 0;
 int value_poty = 0;
 int posx = 0;
 int value_potx = 0;
-const int max_roty = 120;
-const int max_rotx = 170;
+const int max_roty = 1920;
+const int max_rotx = 2720;
 
 void setup_wifi()
 {
@@ -95,17 +97,19 @@ AccelStepper myStepperx(AccelStepper::DRIVER, stepPinx, dirPinx);
 
 void setup() {
 	// OTA
-	//ota.setHostname("espdoolhof");  
-	//ota.setPassword("espdoolhof");
-	//ota.begin();
+	ota.setHostname("espdoolhof");  
+	ota.setPassword("espdoolhof");
+	ota.begin();
 
   //MQTT -
   Serial.begin(115200);
-  //setup_wifi();
-  //client.setServer(MQTT_SERVER, MQTT_PORT);
+  setup_wifi();
+  client.setServer(MQTT_SERVER, MQTT_PORT);
 
   vTaskDelay(100);
   
+
+  pinMode(2,OUTPUT);
   //pin setup
   pinMode(switchy,INPUT_PULLUP);
   pinMode(switchx,INPUT_PULLUP);
@@ -114,23 +118,24 @@ void setup() {
   pinMode(potx,INPUT);
   pinMode(2,OUTPUT);
   //motor setup
-  mySteppery.setMaxSpeed(1000.0);    // must be equal to or greater than desired speed.
-  mySteppery.setSpeed(900.0);       // desired speed to run at
-  mySteppery.setAcceleration(550); // desired acceleration
-  myStepperx.setMaxSpeed(1000.0);    // must be equal to or greater than desired speed.
-  myStepperx.setSpeed(900.0);       // desired speed to run at
-  myStepperx.setAcceleration(550); // desired acceleration
+  mySteppery.setMaxSpeed(16000.0);    // must be equal to or greater than desired speed.
+  mySteppery.setSpeed(16000.0);       // desired speed to run at
+  mySteppery.setAcceleration(9500); // desired acceleration
+  myStepperx.setMaxSpeed(16000.0);    // must be equal to or greater than desired speed.
+  myStepperx.setSpeed(16000.0);       // desired speed to run at
+  myStepperx.setAcceleration(9500); // desired acceleration
+  //standard value of speed 14400 and acceleration 8800
   Serial.println("begin setup");
   //find home position
   while(!digitalRead(switchy)){
 	if (!mySteppery.run()){
-		mySteppery.move(5);
+		mySteppery.move(-5);
 	}
 	taskYIELD(); //needed for OTA to work properly (otherwise OTA update will fail) since OTA is running on a different task
   }
   while(!digitalRead(switchx)){
 	if (!myStepperx.run()){
-		myStepperx.move(-5);
+		myStepperx.move(5);
 
 	}
 	taskYIELD();
@@ -142,44 +147,54 @@ void setup() {
   posy = 0;
   posx = 0;
   Serial.println("setup done");
+   if (!client.connected())
+    {
+        reconnect();
+    }
+    client.loop();
+  //flashing led for visual feedback
+  digitalWrite(2, HIGH);
+  vTaskDelay(500);
+  digitalWrite(2, LOW);
+  vTaskDelay(500);
 }
 
 
 
 void loop() {
 	//MQTT -
-    //if (!client.connected())
-    //{
-    //    reconnect();
-    //}
-    //client.loop();
+    if (!client.connected())
+    {
+        reconnect();
+    }
+    client.loop();
 
-    // client.publish(topic, "datum"); // deze nog plaatsen waar gedetecteerd wordt dat balletje in het juiste gat is gevallen (en "datum" veranderen)
+     //client.publish(topic, "datum"); // deze nog plaatsen waar gedetecteerd wordt dat balletje in het juiste gat is gevallen (en "datum" veranderen)
     //- MQTT
 
 	//mysteppery.stop() //niet nodig denk ikkkkkkkkkkkkkkk
 	//while(!digitalRead(finish)){
     //move x to the measured position of the potentiometer
+    //digitalWrite(2, HIGH);
 		value_potx = analogRead(potx);
 		posx = (value_potx*max_rotx)/4095;
-		myStepperx.moveTo(posx);
-		myStepperx.run();
-    Serial.println(posx);
+    //if(!myStepperx.run()){
+      myStepperx.moveTo(posx);
+    //}
+    myStepperx.run();
 		// move y to the measured position of the potentiometer
 		value_poty = analogRead(poty);
 		posy = -(value_poty*max_roty)/4095;
-		mySteppery.moveTo(posy);
-		mySteppery.run();
-    Serial.println(posy);
+    mySteppery.moveTo(posy);
+    mySteppery.run();
+
 
     //visuel feedback of end of loop function
-		//digitalWrite(2, HIGH);
+		
 		//delay(50);
 		//digitalWrite(2, LOW);
 		//delay(50);
-
 		taskYIELD();
-		delay(50);
 	
   	//}
     //mogelijks eerst connecten en dan pas publishen om te vermijden dat er iets verloren gaat
